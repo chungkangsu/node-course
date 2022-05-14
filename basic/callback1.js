521cf962b0bf457b0944414210dd4ffb34a780c0
Dylan <dylan.su1109@email.com>
2022-05-14 16:08:16 +0800

    add

--- basic/callback1.js
+++ basic/callback1.js
@@ -1,4 +1,3 @@
-
 let dt = new Date();
 console.log(`起床了 at ${dt.toISOString()}`);
 
@@ -8,18 +7,19 @@ let doWork = function (job, timer, cb) {
     let result = `完成工作: ${job} at ${dt.toISOString()}`;
     cb(result);
   }, timer);
-//   console.log(`在 setTimeout 之後 ${job}`);
+  //   console.log(`在 setTimeout 之後 ${job}`);
 };
 
 // 刷牙 (3000) -> 吃早餐 (5000) -> 寫功課 (3000)
 
-doWork('刷牙', 3000, function (result) {
+doWork("刷牙", 3000, function (result) {
+  console.log(result);
+  doWork("吃早餐", 5000, function (result) {
     console.log(result);
-    doWork('吃早餐', 5000, function (result) {
+    doWork("寫功課", 3000, function (result) {
       console.log(result);
-      doWork('寫功課', 3000, function (result) {
-        console.log(result);
-      });
     });
+  });
 });
+
 
