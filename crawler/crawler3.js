44d53030970e1c69983666ee90e8262ef189d3bd
Dylan <dylan.su1109@email.com>
2022-05-16 16:27:50 +0800

    add crawler3.js

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -1,1 +1,22 @@
-const axios = require('axios');
+const axios = require("axios");
+const fs = require("fs/promises");
+
+(async () => {
+  try {
+    const stockNo = await fs.readFile("stock.txt", "utf-8");
+    const response = await axios.get(
+      "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+      {
+        params: {
+          // 設定 query string
+          response: "json",
+          date: "20220301",
+          stockNo: stockNo,
+        },
+      }
+    );
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+})();
