1c03167fe87e655a18d4567860aafe44cbbf5e56
Dylan <dylan.su1109@gmail.com>
2022-05-28 17:26:13 +0800

    0528

--- simple-express/server.js
+++ simple-express/server.js
@@ -1,12 +1,60 @@
-const express = require("express");
+const express = require('express');
 
 const app = express();
+const cors = require('cors');
+app.use(cors());
+const mysql = require('mysql2/promise');
+require ('dotenv').config();
+
+let pool = mysql
+  .createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+   
+    connectionLimit: 10,
+  })
+  
 
 app.get("/", (request, response, next) => {
-  response.send("首頁");
+  console.log("2")
+  
+  
+  response.send("首頁")
 });
+
+
+
 app.get("/about", (request, response, next) => {
   response.send("About Me");
+});
+
+app.get('/stocks', async (request, response, next) => {
+  
+  let [data, fields] = await pool.execute('SELECT * FROM stocks');
+ 
+  response.json(data);
+  
+});
+
+app.get('/stocks/:stockId', async (request, response, next) => {
+  // console.log('query stock by id:', data);
+  let [data, fields] = await pool.execute(
+    'SELECT * FROM stocks WHERE id =' + request.params.stockId
+  );
+  console.log('query stock by id:', data);
+  response.json(data);
+});
+
+app.use((request, response, next)=>{
+  console.log("404",request.path)
+  response.status(404).send('not found')
+});
+
+app.use((err,request, response, next)=>{
+  console.log("四參數",request.path,err)
+  response.status(500).send('server err')
 });
 
 app.listen(3001, () => {
