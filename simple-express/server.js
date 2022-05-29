6542e84594c5163c9854c3cedf5d9bceae968c84
Dylan <dylan.su1109@email.com>
2022-05-29 16:29:50 +0800

    0529

--- simple-express/server.js
+++ simple-express/server.js
@@ -39,13 +39,50 @@ app.get('/stocks', async (request, response, next) => {
 });
 
 app.get('/stocks/:stockId', async (request, response, next) => {
-  // console.log('query stock by id:', data);
-  let [data, fields] = await pool.execute(
-    'SELECT * FROM stocks WHERE id =' + request.params.stockId
+  //1. 取得目前在第幾頁，而且利用 || 這個特性來做預設值
+  let page = request.query.page||1;
+  console.log('current page', page);
+
+  // 2. 取得目前的總筆數
+  let [allResults,fields] = await pool.execute(
+    'SELECT * FROM stock_prices WHERE stock_id = ?' ,[request.params.stockId]
   );
-  console.log('query stock by id:', data);
-  response.json(data);
+  const total = allResults.length;
+  console.log('total:', total);
+
+  // 3. 計算總共有幾頁
+  const perPage = 5; // 每一頁有幾筆
+  const lastPage = Math.ceil(total / perPage);
+  console.log('lastPage:', lastPage);
+
+  // 4. 計算 offset 是多少（計算要跳過幾筆）
+  // 在第五頁，就是要跳過 4 * perPage
+  let offset = (page - 1) * perPage;
+  console.log('offset:', offset);
+
+  // 5. 取得這一頁的資料 select * from table limit ? offet ?
+  let [pageResults] = await pool.execute('SELECT * FROM stock_prices WHERE stock_id = ? ORDER BY date DESC LIMIT ? OFFSET ?', [request.params.stockId, perPage, offset]);
+
+
+
+
+  
+  //回給前端
+  response.json({
+    
+    pagination: {
+      total,
+      lastPage,
+      page,
+    },
+
+    data: pageResults,
+  });
+    
+
 });
+
+
 
 app.use((request, response, next)=>{
   console.log("404",request.path)
